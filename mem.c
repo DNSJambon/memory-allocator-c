a1a37856c87b6b411ce4b4dec7653711b7a8dedc
Matthieu Muller <matthieu.muller2@etu.univ-grenoble-alpes.fr>
2023-12-07 14:07:42 +0100

    free

--- mem.c
+++ mem.c
@@ -187,11 +187,27 @@ void *prev_fb(void *ptr){
 }
 
 void mem_free(void *mem) {
-    if (is_fb(((prev_fb(mem))))==0 & is_fb(((void*)mem+ sizeof(struct ub) + ((struct ub*)mem)->size))==0){
+    if (is_fb(((prev_b(mem))))==0 & is_fb(((void*)mem+ sizeof(struct ub) + ((struct ub*)mem)->size))==0){ /*check si ub.mem.ub*/
+        struct fb *free_fb;
+        free_fb =mem;
+        free_fb->next=((struct fb*)prev_fb(mem))->next;
+        ((struct fb*)prev_fb(mem))->next = free_fb;
+    }
+    else if (is_fb(((prev_b(mem))))==1 & is_fb(((void*)mem+ sizeof(struct ub) + ((struct ub*)mem)->size))==0){ /*check si fb.mem.ub*/
 
     }
+    else if (is_fb(((prev_b(mem))))==0 & is_fb(((void*)mem+ sizeof(struct ub) + ((struct ub*)mem)->size))==1){ /*check si ub.mem.fb*/
+
+    }
+    else { /*il reste fb.mem.fb*/
+
+    }
+
 }
 
+void* prev_b(void * ptr ) {
+    return ptr;
+}
 
 struct fb *mem_fit_first(struct fb *list, size_t size) {
     struct fb *ptr_fb = list;
